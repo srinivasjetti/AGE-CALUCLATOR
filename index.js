a01bb26cd3efb4b5dc546a3eec3eb377ce3bd13a
srinivasjetti <95925735+srinivasjetti@users.noreply.github.com>
2022-07-05 21:57:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let function age() {
+     function age() {
     let  d1 = document.getElementById('date').value;
     let  m1 = document.getElementById('month').value;
     let  y1 = document.getElementById('year').value;
