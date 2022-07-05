aa4acef65c02281acd2f6a37d4faf954ef247961
srinivasjetti <95925735+srinivasjetti@users.noreply.github.com>
2022-07-05 22:05:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
-    <head>
-        <link href="style.css" rel="stylesheet">
-    </head>
 <body>
 <div class="container">
   <form>
@@ -30,33 +27,34 @@
   </div>
     <div id="age"></div>
   </form>
-<script>
-function age() {
-    let  d1 = document.getElementById('date').value;
-    let  m1 = document.getElementById('month').value;
-    let  y1 = document.getElementById('year').value;
-  
-    let  date = new Date();
-    let  d2 = date.getDate();
-    let  m2 = 1 + date.getMonth();
-   let  y2 = date.getFullYear();
-   let  month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  
-    if(d1 > d2){
-      d2 = d2 + month[m2 - 1];
-      m2 = m2 - 1;
-    }
-    if(m1 > m2){
-      m2 = m2 + 12;
-      y2 = y2 - 1;
-    }
-    let  d = d2 - d1;
-   let  m = m2 - m1;
-    let  y = y2 - y1;
-  
-    document.getElementById('age').innerHTML = 'Your Age is '+y+' Years '+m+' Months '+d+' Days';
+ <script type="text/javascript">
+  function age() {
+  let  d1 = document.getElementById('date').value;
+  let  m1 = document.getElementById('month').value;
+  let  y1 = document.getElementById('year').value;
+
+  let  date = new Date();
+  let  d2 = date.getDate();
+  let  m2 = 1 + date.getMonth();
+ let  y2 = date.getFullYear();
+ let  month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  if(d1 > d2){
+    d2 = d2 + month[m2 - 1];
+    m2 = m2 - 1;
   }
- </script>
- </div>
-  </body>
-  </html>
+  if(m1 > m2){
+    m2 = m2 + 12;
+    y2 = y2 - 1;
+  }
+  let  d = d2 - d1;
+ let  m = m2 - m1;
+  let  y = y2 - y1;
+
+  document.getElementById('age').innerHTML = 'Your Age is '+y+' Years '+m+' Months '+d+' Days';
+}
+</script>
+</div>
+</body>
+</html>
+
